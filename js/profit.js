1c4a94a5d4b141df7683a7b46768b0539e68e110
Henahax <mail@henahax.de>
2018-04-29 00:04:24 +0200

    Profit Design

--- js/profit.js
+++ js/profit.js
@@ -1,21 +1,21 @@
 function values(){
-    var ectoEinkauf = 0;
-    var staubVerkauf = 0;
+    var ectoBuy = 0;
+    var dustSell = 0;
     $.getJSON('https://api.guildwars2.com/v2/commerce/prices?ids=19721,24277', function (data) {
         $.each(data, function (index, element) {
             if (element.id == 19721) {
-                ectoEinkauf = element.buys.unit_price;
+                ectoBuy = element.buys.unit_price;
             }
             if (element.id == 24277) {
-                staubVerkauf = element.sells.unit_price;
+                dustSell = element.sells.unit_price;
             }
         });
         var profit = 0;
-        profit = Math.round(((1.85 * staubVerkauf) / 1.15) - (ectoEinkauf + 50));
+        profit = Math.round(((1.85 * dustSell) / 1.15) - (ectoBuy + 50));
 
-        document.getElementById("ectoBuy").innerHTML = "<h4>" + ectoEinkauf + " " + "<img src='https://wiki.guildwars2.com/images/e/eb/Copper_coin.png'>" + "</h4>";
-        document.getElementById("dustSell").innerHTML = "<h4>" + staubVerkauf + " " + "<img src='https://wiki.guildwars2.com/images/e/eb/Copper_coin.png'>" + "</h4>";
-        document.getElementById("profit").innerHTML = "<h4>" + profit + " " + "<img src='https://wiki.guildwars2.com/images/e/eb/Copper_coin.png'>" + "</h4>";
+        document.getElementById("ectoBuy").innerHTML = ectoBuy + " " + "<img src='https://wiki.guildwars2.com/images/e/eb/Copper_coin.png'>";
+        document.getElementById("dustSell").innerHTML = dustSell + " " + "<img src='https://wiki.guildwars2.com/images/e/eb/Copper_coin.png'>";
+        document.getElementById("profit").innerHTML = profit + " " + "<img src='https://wiki.guildwars2.com/images/e/eb/Copper_coin.png'>";
     });
 
 }
