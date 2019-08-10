be1f5b45e32ab3c9a5d84f16e412c62b4b3288fb
Henahax <mail@henahax.de>
2019-08-10 16:55:58 +0200

    Wartung

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function home() {
     document.getElementById("contentHome").style.display = "block";
-//  document.getElementById("contentTools").style.display = "none";
+  document.getElementById("contentTools").style.display = "none";
     document.getElementById("contentDiscord").style.display = "none";
 
     $('a#navhome').addClass('active');
-//  $('a#navtools').removeClass('active');
+  $('a#navtools').removeClass('active');
     $('a#navdiscord').removeClass('active');
 
     copyright();
 }
 
-/*
-function tools(){
+
+/* function tools(){
     document.getElementById("contentHome").style.display = "none";
     document.getElementById("contentTools").style.display = "block";
     document.getElementById("contentDiscord").style.display = "none";
@@ -23,26 +23,26 @@ function tools(){
     copyright();
     values();
 }
-*/
+
 
 function discord(){
     document.getElementById("contentHome").style.display = "none";
-//  document.getElementById("contentTools").style.display = "none";
+ document.getElementById("contentTools").style.display = "none";
     document.getElementById("contentDiscord").style.display = "block";
 
     $('a#navhome').removeClass('active');
-//  $('a#navtools').removeClass('active');
+ $('a#navtools').removeClass('active');
     $('a#navdiscord').addClass('active');
 
     copyright();
-}
+} */
 
 function copyright(){
     document.getElementById("copyright").innerHTML = "© " + new Date().getFullYear() + " - Henahax"; 
 }
 
-/*
-function values(){
+
+/* function values(){
     var ectoBuy = 0;
     var dustSell = 0;
     $.getJSON('https://api.guildwars2.com/v2/commerce/prices?ids=19721,24277', function (data) {
@@ -61,5 +61,4 @@ function values(){
         document.getElementById("dustSell").innerHTML = dustSell + " " + "<img src='https://wiki.guildwars2.com/images/e/eb/Copper_coin.png'>";
         document.getElementById("profit").innerHTML = profit + " " + "<img src='https://wiki.guildwars2.com/images/e/eb/Copper_coin.png'>";
     });
-}
-*/
+} */
