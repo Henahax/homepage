c490c577e0b6483f5b96f29ac3ff9c0c56691758
Henahax <mail@henahax.de>
2022-10-06 09:20:43 +0200

    Homepage 2022 Start

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 function home() {
-  document.getElementById("contentHome").style.display = "block";
-  //document.getElementById("contentTools").style.display = "none";
-  //document.getElementById("contentDiscord").style.display = "none";
 
-  $('a#navhome').addClass('active');
-  //$('a#navtools').removeClass('active');
-  //$('a#navdiscord').removeClass('active');
 }
