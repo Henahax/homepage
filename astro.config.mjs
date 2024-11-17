3eff6d6554c213b3365f15339d3dba5245cc4f23
Henahax <mail@henahax.de>
2024-11-17 12:37:19 +0100

    rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+    integrations: [react(), tailwind({
+        applyBaseStyles: false,
+    }),]
 });
