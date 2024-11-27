a94af011a9f44e7bd668b7725d7fdd2b3036ae7b
Henahax <mail@henahax.de>
2024-11-27 01:07:00 +0100

    game

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -3,17 +3,21 @@ import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const TabsComponent: React.FC = () => (
-  <Tabs defaultValue="card1" className="w-[400px]">
-    <TabsList className="grid w-full grid-cols-2">
-      <TabsTrigger value="card1">card1</TabsTrigger>
-      <TabsTrigger value="card2">card2</TabsTrigger>
+  <Tabs defaultValue="deadlock" className="w-fit">
+    <TabsContent value="gw2">
+      <div>Guild Wars 2</div>
+    </TabsContent>
+    <TabsContent value="deadlock">
+      <div>Deadlock</div>
+    </TabsContent>
+    <TabsList className="grid w-full grid-cols-2 h-full p-2 gap-2">
+      <TabsTrigger value="gw2" className="p-0">
+        <img className="size-16 p-2" src="https://media.invisioncic.com/a311129/monthly_2021_04/pKNMJ0DZ2WI1B.thumb.png.c6c32b5f04c39ad01f6993aa50c5d8f8.png"/>
+      </TabsTrigger>
+      <TabsTrigger value="deadlock" className="p-0">
+        <img className="size-16 p-2" src="https://cdn2.steamgriddb.com/icon_thumb/eafd47a145d67a244ac72fa0617c3224.png"/>
+      </TabsTrigger>
     </TabsList>
-    <TabsContent value="card1">
-      <div>card1</div>
-    </TabsContent>
-    <TabsContent value="card2">
-      <div>card2</div>
-    </TabsContent>
   </Tabs>
 );
 
